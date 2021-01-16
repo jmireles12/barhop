786fed6ee620bf5909bfdcf0f54cef53512dd46b
Juan Mireles <jmireles12@gmail.com>
2021-01-16 15:56:34 -0600

    test

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
 import dummyStore from "./store";
 
-export default {
+const config = {
     API_ENDPOINT: dummyStore,
     API_KEY: process.env.REACT_APP_API_KEY,
     API_SEARCH: `https://maps.googleapis.com/maps/api/place/textsearch/json?key=AIzaSyB5zppx2H_TdbclNzQJMsEU-iOFP930_vE&type=bar`,
 }
+
+export default config
