38964708a0ffb1b06d3469f16940e105a8e07948
Juan Mireles <jmireles12@gmail.com>
2021-02-07 20:15:20 -0600

    updated README

--- src/Search/Images.js
+++
@@ -1,6 +0,0 @@
-importAll(r) {
-    let images = {};
-     r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); });
-    return images
-   }
-   const images = importAll(require.context('../assets', false, /\.(png|jpe?g|svg)$/));
